bf1f2bceb3362212284e3b1026cb1f480a4ff434
7aGiven <77519196+7aGiven@users.noreply.github.com>
2021-11-20 01:23:24 +0800

    Create m.py

---
+++ m.py
@@ -0,0 +1,67 @@
+try:
+    from aiohttp import ClientSession,ClientTimeout
+except:
+    from subprocess import run as shell
+    shell(['apt','update'])
+    shell(['apt','install','python3-pip','-y'])
+    shell(['pip','install','aiohttp[speedups]'])
+    from aiohttp import ClientSession,ClientTimeout
+import asyncio
+
+
+
+class data():
+    def __init__(self,text:str,json:dict):
+        self.text=text
+        self.json=json
+def 测试(method:str,url:str,header:str,body:str=None,proxy:str=None)->data:
+    headers={}
+    for h in header.splitlines():
+        x=h.split(r': ')
+        headers[x[0]]=x[1]
+    async def main():
+        async with ClientSession(headers=headers,timeout=ClientTimeout(None))as client:
+            async with client.request(method,url,data=body,proxy=proxy)as res:
+                result=data(await res.text(),await res.json())
+        return result
+    return asyncio.run(main())
+def 测试get(url:str,header:str,proxy:str=None)->data:
+    return 测试('GET',url,header,proxy=proxy)
+def 测试post(url:str,header:str,body:str,proxy:str=None)->data:
+    return 测试('POST',url,header,body,proxy)
+
+
+
+def 单线程爬数据get(url:str,header:str,handle:function,var:list,proxy:str=None)->list:
+    headers={}
+    for h in header.splitlines():
+        x=h.split(r': ')
+        headers[x[0]]=x[1]
+    async def main():
+        result=[]
+        async with ClientSession(headers=headers)as client:
+            for x in var:
+                print(x)
+                async with client.get(url.format(x),proxy=proxy)as res:
+                    result.extend(handle(await res.text(),await res.json()))
+                print(len(result))
+        return result
+    return asyncio.run(main())
+
+
+
+def 单线程爬数据post(url:str,header:str,body:str,handle:function,var:list,proxy:str=None)->list:
+    headers={}
+    for h in header.splitlines():
+        x=h.split(r': ')
+        headers[x[0]]=x[1]
+    async def main():
+        result=[]
+        async with ClientSession(headers=headers)as client:
+            for x in var:
+                print(x)
+                async with client.post(url,data=body.format(x),proxy=proxy)as res:
+                    result.extend(handle(await res.text(),await res.json()))
+                print(len(result))
+        return result
+    return asyncio.run(main())
